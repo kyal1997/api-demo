758e9552963e00eab81f0f4a6f49943b5c806cb2
Your Name <you@example.com>
2022-10-04 10:55:19 +0100

    Weather api get request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
+  const [weather, setWeather] = useState("");
+
+  const getWeatherLDN = () => {
+    axios.get("http://api.openweathermap.org/data/2.5/forecast?q=London&type=hour&start=23&cnt=3&appid=3e59df4403ab714e43c3be93b1d8dda9").then(
+      (response)=> {
+        console.log(response);
+        setWeather("Temp: " + response.data.list[0].main.temp + "K Conditions: " + response.data.list[0].weather[0].description);
+      }
+    );
+  };
+
+  const getWeatherLeeds = () => {
+    axios.get("http://api.openweathermap.org/data/2.5/forecast?q=Leeds&type=hour&start=23&cnt=3&appid=3e59df4403ab714e43c3be93b1d8dda9").then(
+      (response)=> {
+        console.log(response);
+        setWeather("Temp: " + response.data.list[0].main.temp + "K Conditions: " + response.data.list[0].weather[0].description);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getWeatherLDN}>Osterley</button>
+      <button onClick={getWeatherLeeds}>Leeds</button>
+      {weather}
     </div>
   );
 }
